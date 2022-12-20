af2cc07ea39b879a45a3db545f1901f5783d8ec7
SK <sarthakkarandikar03@gmail.com>
2022-12-20 05:52:08 +0000

    Dec 20, 2022, 11:22 AM

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import Navbar from "./components/Navbar";
 
 const App = () => {
+  
   return (
     <Fragment>
       <Navbar loginSuccessful={onLoginHandler} onLogout={onLogoutHandler}>
