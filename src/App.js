7d1e847e996f9a4ad50dbf5d697ab599e0b44c91
SK <sarthakkarandikar03@gmail.com>
2022-12-20 05:51:11 +0000

    Dec 20, 2022, 11:21 AM

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Navbar loginSuccessful={onLoginHandler} l
+    <Navbar loginSuccessful={onLoginHandler} onLogout={on}
   )
 }
 
