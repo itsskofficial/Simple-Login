e3681e5d126c7893a5f6050c8b4a3fb61df0686e
SK <sarthakkarandikar03@gmail.com>
2022-12-20 08:53:44 +0000

    Dec 20, 2022, 2:23 PM

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     const storedUserInformation =localStorage.getItem('user')
 
     if (storedUserInformation != null) {
-      
+      setCurrentUser(storedUserInf)
     }
   })
   
