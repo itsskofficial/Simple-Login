45ff1decb578bee434db89e93d2ea134ddd8f0a4
SK <sarthakkarandikar03@gmail.com>
2022-12-20 05:56:07 +0000

    Dec 20, 2022, 11:26 AM

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./components/Navbar";
 const App = () => {
 
   const [currentUser, setCurrentUser] = useState(null)
-  const [loginState, setLoginState] = useState(false)
   
   
   
