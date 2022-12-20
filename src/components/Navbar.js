13338d4ee43832c1f3df75776e2dd4df684b5bbe
SK <sarthakkarandikar03@gmail.com>
2022-12-20 05:35:51 +0000

    Dec 20, 2022, 11:05 AM

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import { Fragment, useState } from "react"
 const Navbar = (props) => {
     const [navbarContents, setnavbarContents] = useState(
         <div className="navbar-container">
-                <h1 className="navbar-title">
-                    Welcome
-                </h1>
+            <h1 className="navbar-title">
+                Welcome
+            </h1>
         </div>
         
     )
