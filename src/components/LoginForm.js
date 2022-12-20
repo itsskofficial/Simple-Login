91212122ca9b5ad906a259f20e2398ca788d580b
SK <sarthakkarandikar03@gmail.com>
2022-12-20 06:00:09 +0000

    Dec 20, 2022, 11:30 AM

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -94,7 +94,7 @@ const LoginForm = (props) => {
     return (
         <Fragment>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorConfirmHandler}/>}
-            {loginState?}
+            {loginState?loginForm:}
         </Fragment>
     )
 }
