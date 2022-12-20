ae77ad376b5f48ab0dca78bb7df1bc0108bb2be5
SK <sarthakkarandikar03@gmail.com>
2022-12-20 08:18:24 +0000

    Dec 20, 2022, 1:48 PM

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = (props) => {
         <div className="navbar-container">
                     <div className="navbar-title-container">
                         <h1 className="navbar-title">
-                            Hello {props.user.username}
+                            Hello {ifprops.user.username}
                         </h1>
                     </div>
                     <div className="navbar-actions">
