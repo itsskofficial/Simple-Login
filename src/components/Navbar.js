7009f0e22b5f5102691268deab2d82c9cbfe8b2e
SK <sarthakkarandikar03@gmail.com>
2022-12-20 05:35:12 +0000

    Dec 20, 2022, 11:05 AM

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { Fragment, useState } from "react"
 
 const Navbar = (props) => {
     const [navbarContents, setnavbarContents] = useState(
-        <div className></div>
+        <div className=""></div>
     )
     return (
         <Fragment>
