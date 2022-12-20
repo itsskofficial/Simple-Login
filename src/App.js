bd313c2eb797d4befc6ccd3874cbb04e5ec95d66
SK <sarthakkarandikar03@gmail.com>
2022-12-20 09:04:03 +0000

    Dec 20, 2022, 2:34 PM

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 
   useEffect(()=>{
     const storedUserInformation = localStorage.getItem('user')
-    console.log(storedUserInformation)
+    console.log(JSON>storedUserInformation)
 
     if (storedUserInformation != null) {
       setCurrentUser(storedUserInformation)
