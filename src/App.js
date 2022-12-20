34e3bf6d38f6886b4736c95ca85d56369ae77131
SK <sarthakkarandikar03@gmail.com>
2022-12-20 08:53:00 +0000

    Dec 20, 2022, 2:23 PM

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   
   const onLoginHandler = (user) => {
     setCurrentUser(user)
-    localStorage.setItem('user',user)
+    localStorage.setItem('user',currentUser)
   }
 
   const onLogoutHandler = (logout) => {
