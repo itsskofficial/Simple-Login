8c7423cad3e5b7965c980f6e903564c9bc283eea
SK <sarthakkarandikar03@gmail.com>
2022-12-20 06:03:02 +0000

    Dec 20, 2022, 11:33 AM

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -94,7 +94,7 @@ const LoginForm = (props) => {
     return (
         <Fragment>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorConfirmHandler}/>}
-            ?loginForm:<h1 className="after-login-text">Hahahhaa</h1>}
+            { props.user?loginForm:<h1 className="after-login-text">Hahahhaa</h1>}
         </Fragment>
     )
 }
