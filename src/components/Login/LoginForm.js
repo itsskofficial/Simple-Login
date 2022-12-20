716b1fd5d7053fd1e9a2c31c28a15e550dc92d85
SK <sarthakkarandikar03@gmail.com>
2022-12-20 05:17:32 +0000

    Dec 20, 2022, 10:47 AM

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -60,13 +60,13 @@ const LoginForm = (props) => {
                 <div className="login-form-container">
                     <form onSubmit={formSubmitHandler}>
                         <div className="login-form-username-container">
-                        <label for="login-form-username-input" className="login-form-username-label">
-                            Enter Username
-                        </label>
-                        <input type="text" id="login-form-username-input" className="login-form-username-input" ref={userName}>
-                        </input>
+                            <label for="login-form-username-input" className="login-form-username-label">
+                                Enter Username
+                            </label>
+                            <input type="text" id="login-form-username-input" className="login-form-username-input" ref={userName}>
+                            </input>
                         </div>
-                       
+                       <div className="l"
                         <label for="login-form-password-input" className="login-form-password-label">
                             Enter Password
                         </label>
