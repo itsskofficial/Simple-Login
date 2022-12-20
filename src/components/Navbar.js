0e46ed7bd74e480e76bec4dd7384b251c35331a3
SK <sarthakkarandikar03@gmail.com>
2022-12-20 05:23:36 +0000

    Dec 20, 2022, 10:53 AM

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,10 @@
 import { Fragment } from "react"
 
 const Navbar = (props) => {
+    
     return (
         <Fragment>
-            <div className="navbar-container">
-                <h1 className="navbar-title">
-                    Welcome
-                </h1>
-            </div>
+            
         </Fragment>
     )
 }
