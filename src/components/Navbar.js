737e92cd48198f335fcd3e699e0b2629edb89821
SK <sarthakkarandikar03@gmail.com>
2022-12-20 08:03:46 +0000

    Dec 20, 2022, 1:33 PM

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,43 +25,43 @@ const Navbar = (props) => {
                     </div>
         </div>
     
-    const [navbarContents, setnavbarContents] = useState(
-        <div className="navbar-container">
-            <h1 className="navbar-title">
-                Welcome
-            </h1>
-        </div>
-    )
+    // const [navbarContents, setnavbarContents] = useState(
+    //     <div className="navbar-container">
+    //         <h1 className="navbar-title">
+    //             Welcome
+    //         </h1>
+    //     </div>
+    // )
 
-    const logoutHandler = (event) => {
-        setnavbarContents(
-            <div className="navbar-container">
-                <div className="navbar-title-container">
-                    <h1 className="navbar-title">
-                        Welcome
-                    </h1>
-                </div>
-            </div>
-        )
-        props.onLogout(true)
-    }
+    // const logoutHandler = (event) => {
+    //     setnavbarContents(
+    //         <div className="navbar-container">
+    //             <div className="navbar-title-container">
+    //                 <h1 className="navbar-title">
+    //                     Welcome
+    //                 </h1>
+    //             </div>
+    //         </div>
+    //     )
+    //     props.onLogout(true)
+    // }
 
-    if (props.user!=null) {
-        setnavbarContents(
-            <div className="navbar-container">
-                <div className="navbar-title-container">
-                    <h1 className="navbar-title">
-                        Hello {props.user.username}
-                    </h1>
-                </div>
-                <div className="navbar-actions">
-                    <Button onClick={logoutHandler} className="navbar-logout">
-                        Logout
-                    </Button>
-                </div>
-            </div>
-        )
-    }
+    // if (props.user!=null) {
+    //     setnavbarContents(
+    //         <div className="navbar-container">
+    //             <div className="navbar-title-container">
+    //                 <h1 className="navbar-title">
+    //                     Hello {props.user.username}
+    //                 </h1>
+    //             </div>
+    //             <div className="navbar-actions">
+    //                 <Button onClick={logoutHandler} className="navbar-logout">
+    //                     Logout
+    //                 </Button>
+    //             </div>
+    //         </div>
+    //     )
+    // }
     
     return (
         <Fragment>
