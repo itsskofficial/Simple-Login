fb4aede29853b5863065b6dc47fe98bd7e024d06
SK <sarthakkarandikar03@gmail.com>
2022-12-20 04:47:07 +0000

    Dec 20, 2022, 10:17 AM

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -11,7 +11,7 @@ const LoginForm = (props) => {
                     </h2>
                 </div>
                 <div className="login-form-content">
-                    <label for
+                    <label for=""
                 </div>
             </Card>
         </Fragment>
