2ce109fcec0f1705d3925fcf0417988c7ee50680
SK <sarthakkarandikar03@gmail.com>
2022-12-20 04:52:12 +0000

    Dec 20, 2022, 10:22 AM

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -15,6 +15,7 @@ const LoginForm = (props) => {
                     </h2>
                 </div>
                 <div className="login-form-content">
+                <form></form>
                     <label for="login-form-username-input" className="login-form-username-label">
                         Enter Username
                     </label>
