a4424a40e8e2527bb5deb2488942158196b0d0c6
SK <sarthakkarandikar03@gmail.com>
2022-12-20 06:03:54 +0000

    Dec 20, 2022, 11:33 AM

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,7 +9,7 @@ const LoginForm = (props) => {
     const userPassword = useRef()
 
     const [error, setError] = useState(null)
-    loginForm =
+    const loginForm =
                 <Card>
                 <div className="login-form-header">
                     <h2 className="login-form-title">
