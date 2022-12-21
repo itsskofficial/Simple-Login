a97156993be1007f879c588b04a0f55d193079bf
SK <sarthakkarandikar03@gmail.com>
2022-12-21 05:56:43 +0000

    Dec 21, 2022, 11:26 AM

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import AuthContext from "./store/auth-context";
 
 const App = () => {
 
-  const [currentUser, setCurrentUser] = useState(null)
-
-  useEffect(()=>{
-    const storedUserInformation = localStorage.getItem('user')
-    if (storedUserInformation != null) {
-      setCurrentUser(JSON.parse(storedUserInformation))
-    }
-
-  },[])
-  
-  const onLoginHandler = (user) => {
-    setCurrentUser(user)
-    localStorage.setItem('user',JSON.stringify(user))
-  }
-
-  const onLogoutHandler = (logout) => {
-    if (logout) {
-      setCurrentUser(null)
-    }
-  }
+ 
 
   return (
     <AuthContext.Provider value={{ user: currentUser }}>
