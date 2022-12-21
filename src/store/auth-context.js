fe7ae289dbc458e0b64f3d6d1aa80a2bc45f48d6
SK <sarthakkarandikar03@gmail.com>
2022-12-21 05:56:58 +0000

    Dec 21, 2022, 11:26 AM

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -4,4 +4,27 @@ const AuthContext = React.createContext({
     user:null
 })
 
+const AuthContextProvider = (props) => {
+    const [currentUser, setCurrentUser] = useState(null)
+
+    useEffect(()=>{
+      const storedUserInformation = localStorage.getItem('user')
+      if (storedUserInformation != null) {
+        setCurrentUser(JSON.parse(storedUserInformation))
+      }
+  
+    },[])
+    
+    const onLoginHandler = (user) => {
+      setCurrentUser(user)
+      localStorage.setItem('user',JSON.stringify(user))
+    }
+  
+    const onLogoutHandler = (logout) => {
+      if (logout) {
+        setCurrentUser(null)
+      }
+    }
+}
+
 export default AuthContext
